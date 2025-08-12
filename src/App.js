81f8d1f5bea6a624dbc0e02a8a56dc77352994fb
Ayesha3317 <vertexa1.00@gmail.com>
2025-08-12 12:00:32 +0500

    toggle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
 function App() {
+  const [Mode, setMode] = useState(() => {
+    return localStorage.getItem('mode') || 'light';
+  });
+  const toggleMode = () => {
+    if (Mode === 'light') {
+      setMode('dark');
+      localStorage.setItem('mode', 'dark');
+    } else {
+      setMode('light');
+      localStorage.setItem('mode', 'light');
+    }
+  };
+  // useEffect(() => {
+  //   document.body.style.backgroundColor = Mode === 'dark' ? '#042743' : 'white';
+  // }, [Mode]);
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About US"/>
+      <Navbar title="TextUtils" aboutText="About Us" mode={Mode} toggleMode={toggleMode} />
       <div className="container my-3">
-      <TextForm heading = "Enter the text to analyze."/>
-      {/* <About/> */}
+        <TextForm heading="Enter the text to analyze." />
       </div>
     </>
   );
 }
-
 export default App;
